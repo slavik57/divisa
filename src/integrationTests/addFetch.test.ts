8e0bd0a30e1be27af9ec8caa256843f011c8a06c
Slava Shpitalny <slavik57@gmail.com>
2017-04-08 14:51:21 +0300

    Change the fetch to be async and return a promise

--- src/integrationTests/addFetch.test.ts
+++ src/integrationTests/addFetch.test.ts
@@ -1,12 +1,15 @@
 import { stub, SinonStub } from 'sinon';
 import { Cache, CacheCollisionError } from '../index';
 import { expect } from 'chai';
+import * as chai from 'chai';
+import * as chaiAsPromised from 'chai-as-promised';
+chai.use(chaiAsPromised);
 
 describe('Cache', () => {
   describe('add-fetch', () => {
-    it('should return null on empty cache', () => {
+    it('should reject on empty cache', () => {
       const cache = new Cache();
-      expect(cache.fetch('not existing key')).to.be.null;
+      return expect(cache.fetch('not existing key')).to.eventually.be.rejected;
     });
 
     it('should return existing object with same key', () => {
@@ -16,15 +19,15 @@ describe('Cache', () => {
 
       cache.add(key, obj);
 
-      expect(cache.fetch(key)).to.be.equal(obj);
+      return expect(cache.fetch(key)).to.eventually.be.equal(obj);
     });
 
-    it('should return null on not existing key', () => {
+    it('should reject on not existing key', () => {
       const cache = new Cache();
 
       cache.add('some key', {});
 
-      expect(cache.fetch('not existing key')).to.be.null;
+      return expect(cache.fetch('not existing key')).to.eventually.rejected;
     });
 
     it('adding same key should result in error', () => {
